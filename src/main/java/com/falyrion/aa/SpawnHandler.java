714648dd4763e1525f143e0f97442154543a4d69
Falyrion <pascal.bergmann@hotmail.com>
2022-05-10 20:09:23 +0200

    Update SpawnHandler.java

--- src/main/java/com/falyrion/aa/SpawnHandler.java
+++ src/main/java/com/falyrion/aa/SpawnHandler.java
@@ -6,6 +6,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class SpawnHandler implements Listener {
+    
+    /**
+     * Detects if an armor stand has spawned in the world (for example placed by a player) and set its arms to true
+     * This event will be enabled in the onEnable-Method in the main class.
+     *
+     * @param event: CreatureSpawnEvent
+     */
 
     @EventHandler
     public void onArmorStandSpawn(EntitySpawnEvent event) {
